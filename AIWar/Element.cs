5a6e55dd2cde1bf09ef6bc54c539dd713715bffb
cburcier <cburcier@yahoo.fr>
2017-06-05 23:23:02 +0800

    start doing universe next step routine

--- AIWar/Element.cs
+++ AIWar/Element.cs
@@ -6,34 +6,41 @@ using System.Threading.Tasks;
 
 namespace AIWar
 {
-    class Element
+    abstract class Element
     {
+        
+        private iShape _shape;
+        private string _name;
+        private Vector _position;
+
         public Element(string name, iShape shape)
         {
             Name = name;
             Shape = shape;
         }
-        private iShape _shape;
-        private string _name;
-        private Vector _position;
 
         internal iShape Shape { get => _shape; set => _shape = value; }
         public string Name { get => _name; set => _name = value; }
+        internal Vector Position { get => _position; set => _position = value; }
     }
 
-    class StaticElement : Element
+    abstract class StaticElement : Element
     {
         public StaticElement(string name, iShape shape) : base(name, shape) { }
     }
 
-    class MovingElement : Element
+    abstract class MovingElement : Element
     {
+        public Vector tempPosition;
+        private double _weight;
+        private Vector _speed;
+        
         public MovingElement(string name, iShape shape, double weight) : base(name, shape)
         {
             Weight = weight;
         }
-        private double _weight;
-        private Vector _speed;
+
+        public abstract Vector getSelfForceApplied();
 
         internal double Weight { get => _weight; set => _weight = value; }
         internal Vector Speed { get => _speed; set => _speed = value; }
--- AIWar/Universe.cs
+++ AIWar/Universe.cs
@@ -8,19 +8,51 @@ namespace AIWar
 {
     class Universe
     {
-        private List<MovingElement> _mElemList;
-        private List<StaticElement> _sElemList;
+        //universe config
+        private double _startReversionCoeff;
 
-        public void processStep()
+        private List<MovingElement> _mElemList = new List<MovingElement>();
+        private List<StaticElement> _sElemList = new List<StaticElement>();
+
+        public Universe()
+        {
+            //config
+            StartReversionCoeff = 10;
+        }
+
+        public double StartReversionCoeff { get => _startReversionCoeff; set => _startReversionCoeff = value; }
+
+        public void ProcessStep(int timeStep)
         {
             foreach (MovingElement me in _mElemList)
             {
-                //get the forces 
+                //get the forces
+                Vector force = GetForces(me);
                 //get the next Position 
-                //check collision/damages 
-                //update speed
+                me.Speed = me.Speed + me.Weight * force * timeStep;
+                me.tempPosition = me.Speed * timeStep;
             }
-            
+            foreach (MovingElement me in _mElemList)
+            {
+                //check collision/damages 
+            }
+        }
+
+        public Vector GetForces(MovingElement me)
+        {
+            // acceleration propre de l'element
+            Vector force = me.getSelfForceApplied();
+            // frottement de demarage
+            Vector startReversion = force/force.norm() * me.Weight * Math.Exp(-me.Speed.norm() / StartReversionCoeff);
+            // frottement du terrain
+            Vector frictionForce = -GetFrictionCoeff(me) * me.Speed * me.Weight;
+            return force+startReversion+frictionForce;
+        }
+
+        public double GetFrictionCoeff(MovingElement me)
+        {
+            // peut dependre du type de terrain (fonction de position), du type de l'element (un missile ne roule pas) etc...
+            return 0;
         }
     }
 }
